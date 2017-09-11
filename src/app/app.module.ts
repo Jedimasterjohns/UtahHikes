c316edbf4626ff4da997c5808e7d07f77bfab8e3
Ryan Johns <I59227@xactware.com>
2017-09-11 09:24:25 -0600

    Catching up on all the changes I thought I pushed over the weekend :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes, } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootrstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HikesComponent } from './hikes/hikes.component';
 import { ProtectedComponent } from './protected/protected.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { GlobalEventsManagerService } from './global-events-manager.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertComponent } from './_directives/alert/alert.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { AlertService } from './_services/alert.service';
+import { AuthService } from './_services/auth.service';
+import { UserService } from './_services/user.service';
 import { routing } from './app.routing';
 
 // used to create fake backend
@@ -31,6 +31,7 @@ import { BaseRequestOptions } from '@angular/http';
     ProtectedComponent,
     NavBarComponent,
     LoginComponent,
+    AlertComponent,
     RegisterComponent
   ],
   imports: [
@@ -41,6 +42,15 @@ import { BaseRequestOptions } from '@angular/http';
     routing
   ],
   providers: [
+        AuthGuard,
+        AlertService,
+        AuthService,
+        UserService,
+ 
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
   ],
   bootstrap: [AppComponent]
 })
